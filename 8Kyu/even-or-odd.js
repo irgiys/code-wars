044cf6993d0272d58b1fc638e47a13ea9b741c8f
iynsys <irgiyansy@gmail.com>
2021-08-31 19:05:19 +0700

    add even-or-odd.js

---
+++ 8Kyu/even-or-odd.js
@@ -0,0 +1,8 @@
+const even_or_odd = (number) => (number % 2 == 0 ? "Even" : "Odd");
+
+//test
+console.log(even_or_odd(2)); //, "Even"))));
+console.log(even_or_odd(7)); //, "Odd"))));
+console.log(even_or_odd(-42)); //, "Even"))));
+console.log(even_or_odd(-7)); //, "Odd"))));
+console.log(even_or_odd(0)); //, "Even")
