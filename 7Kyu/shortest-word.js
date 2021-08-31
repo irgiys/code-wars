5f64fadc5a2dde2ce0b7fec66549a8047f00c3c4
iynsys <irgiyansy@gmail.com>
2021-08-31 16:24:13 +0700

    add shortest-word.js

---
+++ 7Kyu/shortest-word.js
@@ -0,0 +1,8 @@
+function findShort(s) {
+   let item = s.split(" ").map((item) => item.length);
+   return Math.min.apply(null, item);
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps")); // 3);
+console.log(findShort("turns out random test cases are easier than writing out basic ones")); // 3);
+console.log(findShort("Let's travel abroad shall we")); // 2);
