e62b5ea1483771e8d0e15d8970b4022c0c1e751d
iynsys <irgiyansy@gmail.com>
2021-09-05 16:17:28 +0700

    edit u-only-beginner.js

--- 8Kyu/u-only-beginner.js
+++ 8Kyu/u-only-beginner.js
@@ -1,12 +1,19 @@
-// const check = (a, x) => {
-//    for (let i = 0; i < a.length; i++) {
-//       if (a[i] === x) return true;
-//    }
-//    return false;
-// };
+const check = (a, x) => {
+   for (let i = 0; i < a.length; i++) {
+      if (a[i] === x) return true;
+   }
+   return false;
+};
 
 const checkSome = (a, x) => a.some((b) => b == x);
 const checkInclude = (a, x) => a.includes(x);
 
 // test
-console.log(checkInclude(["adad", "232", 21, "121"], 21));
+// console.time("Function #1");
+
+var start = +new Date(); // log start timestamp
+
+// console.log(check(["adad", "232", 21, "121"], 21));
+
+var end = +new Date(); // log end timestamp
+var diff = end - start;
