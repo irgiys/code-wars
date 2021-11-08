39159ce12962291f96e6e37f7cf85eae94df64b2
iynsys <irgiyansy@gmail.com>
2021-11-08 11:00:22 +0700

    add hydrated.js

---
+++ 8Kyu/hydrated.js
@@ -0,0 +1,10 @@
+const litres = (time) => Math.floor(time * 0.5);
+
+// test
+console.log(litres(2) /*, 1, 'should return 1 litres'*/);
+console.log(litres(1.4) /*, 0/*, 'should return 0 litres'*/);
+console.log(litres(12.3) /*, 6/*, 'should return 6 litres'*/);
+console.log(litres(0.82) /*, 0/*, 'should return 0 litres'*/);
+console.log(litres(11.8) /*, 5/*, 'should return 5 litres'*/);
+console.log(litres(1787) /*, 893/*, 'should return 893 litres'*/);
+console.log(litres(0) /*, 0, 'should return 0 litres'*/);
