f984d750c76da621b60935037468b043952c74a5
iynsys <Irgiyansy@gmail.com>
2022-03-19 22:25:13 +0700

    add list-filter

---
+++ 7Kyu/list-filter.js
@@ -0,0 +1,8 @@
+function filter_list(l) {
+   // Return a new array with the strings filtered out
+   return l.filter((a) => typeof a !== "string");
+}
+
+console.log(filter_list([1, 2, "a", "b"]));
+console.log(filter_list([1, "a", "b", 0, 15]));
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
